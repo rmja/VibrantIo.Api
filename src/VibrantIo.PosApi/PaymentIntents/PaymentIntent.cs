7853dd122dbf871e3d9015e5507f3ab55b8cf83a
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2025-02-26 13:09:27 +0100

    Add LatestCharge to PaymentIntent

--- src/VibrantIo.PosApi/PaymentIntents/PaymentIntent.cs
+++ src/VibrantIo.PosApi/PaymentIntents/PaymentIntent.cs
@@ -2,9 +2,20 @@
 
 public record PaymentIntent
 {
+    /// <summary>
+    /// Amount in cents
+    /// </summary>
     public int Amount { get; set; }
     public required string Description { get; set; }
+    /// <summary>
+    /// Charge id associated with this payment intent
+    /// </summary>
+    public string? LatestCharge { get; set; }
     public Dictionary<string, string> Metadata { get; set; } = [];
     public PaymentIntentStatus Status { get; set; }
+    /// <summary>
+    /// Id of the terminal used to created this payment intent
+    /// </summary>
+    public required string TerminalId { get; set; }
     public string CancelationReason { get; set; } = "";
 }
