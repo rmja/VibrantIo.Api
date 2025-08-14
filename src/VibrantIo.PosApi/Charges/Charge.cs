633592d37242abca9d99b3072601a083fae3f66b
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2025-08-14 10:27:36 +0200

    Update Charge.cs

--- src/VibrantIo.PosApi/Charges/Charge.cs
+++ src/VibrantIo.PosApi/Charges/Charge.cs
@@ -5,9 +5,12 @@ public record Charge : IPaginateableObject
     public required string Id { get; set; }
     public required string AccountId { get; set; }
     public int Amount { get; set; }
+    public string? BalanceTransactionId { get; set; }
     public required string Currency { get; set; }
     public required string PaymentIntent { get; set; }
     public PaymentStatus Status { get; set; }
+    public string? FailureCode { get; set; }
+    public string? FailureMessage { get; set; }
     public DateTime Created { get; set; }
     public DateTime Updated { get; set; }
     public Dictionary<string, string> Metadata { get; set; } = [];
